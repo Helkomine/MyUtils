99c9e44b17399516265a1570188c5cf831229322
Helkomine <da24rhma@gmail.com>
2026-02-17 15:52:33 +0700

    Update MinimalUpgradeableProxy.sol

--- MinimalUpgradeableProxy.sol
+++ MinimalUpgradeableProxy.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.0;
 contract MinimalUpgradeableProxy {
     bytes32 private constant PROXY_SLOT = 0x881d0b73263d45454e7a85c9f11c3c8199fe13320d9c33bb995d0d9558fd7295;
 
-    constructor(address proxy) payable {
+    constructor(address implementation) payable {
         assembly {
-            sstore(PROXY_SLOT, proxy)
+            sstore(PROXY_SLOT, implementation)
         }
     }
 
