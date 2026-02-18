f22e919c42e4c18cc5f48d1e8f39c24e9062ffe9
Helkomine <da24rhma@gmail.com>
2026-02-18 14:19:16 +0700

    Update MinimalUpgradeableProxy.sol

--- MinimalUpgradeableProxy.sol
+++ MinimalUpgradeableProxy.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.0;
 
-contract MinimalUpgradeableProxy {
+contract ERC7229Proxy {
     bytes32 private constant PROXY_SLOT = 0x881d0b73263d45454e7a85c9f11c3c8199fe13320d9c33bb995d0d9558fd7295;
 
     constructor(address implementation) payable {
