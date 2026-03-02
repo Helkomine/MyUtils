10e3a6e65f51caddaf837005949b6fd5cba55748
Helkomine <da24rhma@gmail.com>
2026-03-02 21:23:40 +0700

    Create Dex2Solution.sol

---
+++ Dex2Solution.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: CC0-1.0
+pragma solidity ^0.8.20;
+interface Dex2 {
+    function swap(address from, address to, uint256 amount) external;
+}
+contract Attack {
+    Dex2 public dex2;
+
+    constructo 
+}
