7b4ffadcde38d20a1289b2c63a7b607cbd0ceb1a
Helkomine <da24rhma@gmail.com>
2026-03-16 11:33:40 +0700

    Update UniswapV3Module.js

--- UniswapV3Module.js
+++ UniswapV3Module.js
@@ -2,7 +2,33 @@
 pragma solidity ^0.8.0;
 
 contract UniswapModule is IUniswapV3SwapCallback {
-    function uniswapV3SwapCallback(int256, int256, bytes calldata data) external {}
+    using SafeERC20 for IERC20;
+    
+    // uniswap.v3.callback-slot - 1
+    bytes32 internal constant CALLBACK_SLOT = 0x6ae0ded922d357ca7d0ce412e5fa8ffb0e0478fa5cd9868cb81a424686141252;
+    
+    function uniswapV3SwapCallback(int256 amount0, int256 amount1, bytes calldata data) external virtual {
+        _processV3SwapCallback(amount0, amount1, data);
+    }
+
+    function _processV3SwapCallback(int256, int256, bytes calldata data) internal virtual {
+        address pool;
+        
+        assembly {
+            pool := tload(CALLBACK_SLOT)
+        }
+
+        require(msg.sender == pool);
+        
+        (address tokenIn, bool zeroForOne) = abi.decode(data, (address, bool));
+        uint256 amount;
+        
+        assembly {
+            amount := calldataload()
+        }
+
+        IERC20(tokenIn).safeTransfer(msg.sender, amount);
+    }
 
     function _v3SwapExactIn(bytes calldata param) internal virtual {
         uint256 factoryId;
@@ -16,6 +42,10 @@ contract UniswapModule is IUniswapV3SwapCallback {
         uint8 slotSet;
 
         assembly ("memory-safe") {
+            if sub(param.length, 0x140) {
+                revert(0, 0)
+            }
+            
             factoryId := calldataload(add(param.offset, 0x20))
             tokenIn := calldataload(add(param.offset, 0x40))
             tokenOut := calldataload(add(param.offset, 0x60))
@@ -56,8 +86,31 @@ contract UniswapModule is IUniswapV3SwapCallback {
                        )
         }
 
+        address pool = IFactory(factory).getPool(tokenIn, tokenOut, fee);
+
         assembly ("memory-safe") {
-            
+            tstore(CALLBACK_SLOT, pool)
+        }
+
+        bool zeroForOne;
+        
+        assembly {
+            zeroForOne := lt(tokenIn, tokenOut)
+            sqrtPriceLimitX96 := sub(MAX_SQRT_RATIO, mul(zeroForOne, DELTA_SQRT_RATIO))
+        }
+
+        IUniswapV3(pool).swap(address(this), zeroForOne, amountSpecified, sqrtPriceLimitX96, abi.encode(tokenIn, zeroForOne));
+
+        assembly ("memory-safe") {
+            tstore(CALLBACK_SLOT, 0)
+        }
+
+        {
+            if (slotSet != 0) {
+                assembly {
+                    tstore(slotSet, amountSpecified)
+                }
+            }
         }
     }
 }
