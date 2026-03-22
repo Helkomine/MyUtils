01c8c313a27a75870fde6c61382b0059ed9446c3
Helkomine <da24rhma@gmail.com>
2026-03-22 20:30:50 +0700

    Update UniswapV2Module.js

--- UniswapV2Module.js
+++ UniswapV2Module.js
@@ -1,19 +1,33 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import '../v2/IUniswapV2Pair.sol';
+import '../v2/IUniswapV2Factory.sol';
+import '../factories/FactoriesPacked.sol';
+
 abstract contract UniswapV2Module {
     function _balanceOf(IERC20 token, address viewer) internal virtual returns (uint256) {
         return token.balanceOf(viewer);
     }
+
+    function _skim(bytes calldata param) internal virtual {
+        // command
+        IUniswapV2Pair pool;
+        assembly {
+            pool := calldataload(add(param.offset, 0x20))
+        }
+
+        pool.skim(address(this));
+    }
     
     function _v2SwapExactIn(bytes calldata param) internal virtual {
-        // bytes2 command
+        // command
         uint256 factoryId;
         address tokenIn;
         address tokenOut;
         uint256 amountOutMin;
         uint8 slotSet;
-
         assembly {
             factoryId := calldataload(add(param.offset, 0x20))
             tokenIn := calldataload(add(param.offset, 0x40))
@@ -22,55 +36,60 @@ abstract contract UniswapV2Module {
             slotSet := calldataload(add(param.offset, 0xa0))
         }
 
+        uint256 beforeBalanceOut = _balanceOf(IERC20(tokenOut), address(this));
+
         address pool;
         {
-            address factory;
-            bytes memory factories = FACTORIES_PACKED;
+            bytes memory factories = FactoriesPacked.FACTORIES_PACKED;
 
+            address factory;
             assembly {
-                factory := shr(
-                    mload(
-                        add(
-                            mul(
-                                factoryId,
-                                20
-                            ),
-                            add(
-                                data,
-                                32
-                            )
-                        )
-                    )
-                )
+                factory := shr(96, mload(add(mul(factoryId, 20), add(factories, 32))))
             }
 
-            pool = IUniswapFactory(factory).getPair(tokenIn, tokenOut);
+            pool = IUniswapV2Factory(factory).getPair(tokenIn, tokenOut);
             if (pool == address(0)) revert();
         }
 
         uint256 amount0;
         uint256 amount1;
         {
-            bool oneForZero = tokenOut < tokenIn;
-            uint256 balanceIn = _balanceOf(tokenIn, pool);
-            uint256 reserveIn;
-            IUniswapV2(pool).getReserves();
+            bool zeroForOne = tokenOut < tokenIn;
+            uint256 balanceIn = _balanceOf(IERC20(tokenIn), pool);
+            IUniswapV2Pair(pool).getReserves();
             assembly {
                 let pointer := mload(0x40)
-                returndatacopy(pointer, 0, 64)
-                reserveIn := mload(add(pointer, shl(5, oneForZero)))
+
+                returndatacopy(pointer, shl(5, zeroForOne), 32)
+                let reserveIn := mload(pointer)
+
                 let amountIn := sub(balanceIn, reserveIn)
                 if lt(balanceIn, amountIn) {
                     revert(0, 0)
                 }
+
+                let reserveOut
                 
-                
+                amountIn := mul(amountIn, 997)
+                let amountOut := div(mul(amountIn, reserveOut), add(amountIn, mul(reserveIn, 1000)))
             }
         }
 
-        IUniswap(pool).swap(amount0, amount1, address(this), new bytes(0));
+        IUniswapV2Pair(pool).swap(amount0, amount1, address(this), new bytes(0));
 
-        if (amount0 > 0) require(amount0 >= amountOutMin);
-        if (amount1 > 0) require(amount1 >= amountOutMin);
+        uint256 afterBalanceOut = _balanceOf(IERC20(tokenOut), address(this));
+
+        uint256 delta = afterBalanceOut - beforeBalanceOut;
+
+        // assert
+        require(delta >= amountOutMin);
+
+        {
+            if (slotSet != 0) {
+                assembly {
+                    tstore(slotSet, delta)
+                }
+            }
+        }
     }
 }
